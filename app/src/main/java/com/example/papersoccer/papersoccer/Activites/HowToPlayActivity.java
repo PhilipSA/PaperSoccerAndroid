b1f0c9a869f4dbf96630a31b69c61620f13b231d
philippp <philip.ppp@live.se>
2017-04-26 16:55:34 +0200

    - Added swipe arrow indicators to the how to play view

--- app/src/main/java/com/example/papersoccer/papersoccer/Activites/HowToPlayActivity.java
+++ app/src/main/java/com/example/papersoccer/papersoccer/Activites/HowToPlayActivity.java
@@ -3,17 +3,55 @@ package com.example.papersoccer.papersoccer.Activites;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+
 import com.example.papersoccer.papersoccer.R;
 
 public class HowToPlayActivity extends AppCompatActivity {
 
+    ImageView leftArrow;
+    ImageView rightArrow;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_how_to_play);
 
-        ViewPager mViewPager = (ViewPager) findViewById(R.id.viewPageAndroid);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        leftArrow = (ImageView)findViewById(R.id.left_nav);
+        rightArrow = (ImageView)findViewById(R.id.right_nav);
+
+        leftArrow.setVisibility(View.INVISIBLE);
+
+        ViewPager mViewPager = (ViewPager)findViewById(R.id.viewpager);
         ImageSliderAdapter adapterView = new ImageSliderAdapter(this);
         mViewPager.setAdapter(adapterView);
+
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if (position == 0)
+                {
+                    leftArrow.setVisibility(View.INVISIBLE);
+                    rightArrow.setVisibility(View.VISIBLE);
+                }
+                else {
+                    leftArrow.setVisibility(View.VISIBLE);
+                    rightArrow.setVisibility(View.INVISIBLE);
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/papersoccer/papersoccer/Activites/ImageSliderAdapter.java
+++ app/src/main/java/com/example/papersoccer/papersoccer/Activites/ImageSliderAdapter.java
@@ -8,12 +8,7 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
 import com.example.papersoccer.papersoccer.R;
-
-/**
- * Created by Admin on 2017-04-26.
- */
 
 public class ImageSliderAdapter extends PagerAdapter {
     Context mContext;
@@ -38,9 +33,7 @@ public class ImageSliderAdapter extends PagerAdapter {
     public Object instantiateItem(ViewGroup container, int i) {
         ImageView mImageView = new ImageView(mContext);
 
-        //mImageView.setImageResource(sliderImagesId[i]);
-        Glide.with(mContext).load(sliderImagesId[i]).into(new GlideDrawableImageViewTarget(mImageView));
-        //Glide.with(this.mContext).load(sliderImagesId[i]).into(mImageView);
+        Glide.with(mContext).load(sliderImagesId[i]).into(mImageView);
 
         ((ViewPager) container).addView(mImageView, 0);
         return mImageView;

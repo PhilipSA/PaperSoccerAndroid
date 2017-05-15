b3c72e7060d8fb8cc241fcd1698c9441e207aed1
philippp <philip.ppp@live.se>
2017-05-15 11:02:11 +0200

    - Fixed crash caused by async type throw

--- app/src/main/java/com/ps/simplepapersoccer/AI/GameAIHandler.kt
+++ app/src/main/java/com/ps/simplepapersoccer/AI/GameAIHandler.kt
@@ -2,17 +2,22 @@ package com.ps.simplepapersoccer.AI
 
 import co.metalab.asyncawait.async
 import com.ps.simplepapersoccer.GameObjects.Game.GameHandler
-import com.ps.simplepapersoccer.GameObjects.Move.PartialMove
 import com.ps.simplepapersoccer.GameObjects.Player.AIPlayer
 
 class GameAIHandler(private val gameHandler: GameHandler, private val calculateAsync: Boolean) {
 
     fun MakeAIMove(aiPlayer : AIPlayer) {
-        var aiMove = if(calculateAsync) MakeAIMoveAsync(aiPlayer) else aiPlayer.gameAI?.MakeMove(gameHandler)
-        gameHandler.AIMakeMove((aiMove as PartialMove?)!!)
+        if(calculateAsync) {
+            MakeAIMoveAsync(aiPlayer)
+            return
+        }
+        else {
+            gameHandler.AIMakeMove(aiPlayer.gameAI?.MakeMove(gameHandler)!!)
+        }
     }
 
     fun MakeAIMoveAsync(aiPlayer : AIPlayer) = async {
-        await { aiPlayer.gameAI?.MakeMove(gameHandler) }
+        var aiMove = await { aiPlayer.gameAI?.MakeMove(gameHandler) }
+        gameHandler.AIMakeMove(aiMove!!)
     }
 }
--- app/src/test/java/test/simplepapersoccer/MinimaxAIUnitTests.kt
+++ app/src/test/java/test/simplepapersoccer/MinimaxAIUnitTests.kt
@@ -1,19 +1,17 @@
 package test.simplepapersoccer
 
-import com.ps.simplepapersoccer.AI.MinimaxAI.MinimaxAI
 import com.ps.simplepapersoccer.Enums.DifficultyEnum
 import com.ps.simplepapersoccer.Enums.GameModeEnum
 import com.ps.simplepapersoccer.GameObjects.Game.GameHandler
 import com.ps.simplepapersoccer.GameObjects.Player.AIPlayer
 import com.ps.simplepapersoccer.GameObjects.Player.Abstraction.IPlayer
+import org.junit.Assert.assertEquals
 
 import org.junit.Test
 import org.junit.Before
 
 class MinimaxAIUnitTests {
-
     var gameHandler: GameHandler? = null
-    var miniMaxAi: MinimaxAI? = null
     var player1: IPlayer? = null
     var player2: IPlayer? = null
 
@@ -21,18 +19,17 @@ class MinimaxAIUnitTests {
     fun init()
     {
         player1 = AIPlayer(DifficultyEnum.Medium, "TestSubject", 1, 0, true)
-        player2 = AIPlayer(DifficultyEnum.Hard, "TestOpponent", 2, 0, true)
+        player2 = AIPlayer(DifficultyEnum.VeryHard, "TestOpponent", 2, 0, true)
 
         var players = arrayListOf(player1!!, player2!!)
 
         gameHandler = GameHandler(null, 10, 12, players, GameModeEnum.AI_VS_AI, false)
-        miniMaxAi = MinimaxAI(2000)
     }
 
     @Test
     @Throws(Exception::class)
     fun minimaxAi_makes_valid_move() {
         gameHandler?.UpdateGameState()
-        assert(player1?.score === 1)
+        assertEquals(player1?.score, 1)
     }
 }

1077c25901ae44b03a4051f3a5b1fd108f7e8657
Philip Sandegren <philip.sandegren@visibacare.com>
2020-05-28 20:35:19 +0200

    New Android studio version 4.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ buildscript {
         google()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:3.4.0")
+        classpath("com.android.tools.build:gradle:4.0.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.30")
         classpath("org.jetbrains.kotlin:kotlin-android-extensions:1.3.30")
     }

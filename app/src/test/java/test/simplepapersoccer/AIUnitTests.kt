08091b0b584ffbee3ea46720f3bf55b221e417e1
Philip Sandegren <philip.sandegren@visibacare.com>
2021-03-04 09:31:38 +0100

    Add different variants of AI matchups in test

--- app/src/test/java/test/simplepapersoccer/AIUnitTests.kt
+++ app/src/test/java/test/simplepapersoccer/AIUnitTests.kt
@@ -2,6 +2,7 @@ package test.simplepapersoccer
 
 import com.ps.simplepapersoccer.ai.neuralnetworkAI.NeuralNetworkAI
 import com.ps.simplepapersoccer.ai.euclideanAI.EuclideanAI
+import com.ps.simplepapersoccer.ai.jonasAI.JonasAI
 import com.ps.simplepapersoccer.gameobjects.game.GameHandler
 import com.ps.simplepapersoccer.gameobjects.player.abstraction.IPlayer
 import junit.framework.TestCase.assertEquals
@@ -36,8 +37,35 @@ class AIUnitTests {
     @Test
     fun neuralNetworkAITraining() {
         val player1 = NeuralNetworkAI(null, 1, 0)
-        val player2: IPlayer = NeuralNetworkAI(null, 2, 1, "temp2.pool")
-        val players = arrayListOf(player1, player2)
+        val player2 = NeuralNetworkAI(null, 2, 1, "temp2.pool")
+        val players = arrayListOf(player1 as IPlayer, player2)
+
+        runTestGame(players)
+
+        player1.neuralNetworkCache.createBackupFile()
+        player2.neuralNetworkCache.createBackupFile()
+
+        assertEquals(0.5, (player1.score.toDouble() / (player1.score + player2.score)))
+    }
+
+    @Test
+    fun neuralNetworkAIVsEuclideanAITraining() {
+        val player1 = NeuralNetworkAI(null, 1, 0)
+        val player2 = EuclideanAI(2, 1)
+        val players = arrayListOf(player1 as IPlayer, player2)
+
+        runTestGame(players)
+
+        player1.neuralNetworkCache.createBackupFile()
+
+        assertEquals(0.5, (player1.score.toDouble() / (player1.score + player2.score)))
+    }
+
+    @Test
+    fun neuralNetworkAIVsJonasAITraining() {
+        val player1 = NeuralNetworkAI(null, 1, 0)
+        val player2 = JonasAI(2, 1)
+        val players = arrayListOf(player1 as IPlayer, player2)
 
         runTestGame(players)
 

5025a081f82e8518c330b4b7a8c4eaac40ba5f71
Philip Sandegren <philip.sandegren@visibacare.com>
2021-03-04 11:24:30 +0100

    Float over Double

--- app/src/main/java/com/ps/simplepapersoccer/Helpers/MathHelper.kt
+++ app/src/main/java/com/ps/simplepapersoccer/Helpers/MathHelper.kt
@@ -5,9 +5,9 @@ import kotlin.math.abs
 import kotlin.math.sqrt
 
 object MathHelper {
-    fun euclideanDistance(point: TwoDimensionalPoint, otherPoint: TwoDimensionalPoint): Double {
+    fun euclideanDistance(point: TwoDimensionalPoint, otherPoint: TwoDimensionalPoint): Float {
         val yCord = abs(point.y - otherPoint.y)
         val xCord = abs(point.x - otherPoint.x)
-        return sqrt((yCord * yCord + xCord * xCord).toDouble())
+        return sqrt((yCord * yCord + xCord * xCord).toFloat())
     }
 }
